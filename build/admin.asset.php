b12abb14aded05956751761105eda097c65d1962
nikolakakis <nikolakakis.manolis@gmail.com>
2026-03-21 09:49:50 +0200

    1.4.2

---
+++ build/admin.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '7eb15688f319186ed636');
---
+++ src/admin.js
@@ -0,0 +1,3 @@
+// src/admin.js
+import './admin/shortcode-wizard.js';
+// You can add more imports here as you create other admin modules
